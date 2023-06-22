3c19456e5dad2fd2b865cc28efa72a99585b6597
OhJaeYoung <ostyoung@naver.com>
2023-06-22 10:05:43 +0900

    SFML-Framework Setting / 미리 컴파일된 헤더

---
+++ stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <random>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+#include <windows.h>
+
+// define, utils
